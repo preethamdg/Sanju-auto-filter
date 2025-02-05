e11368d4bc8a86df101c39d55e41f5b98822d9a1
preethamdg <preethamdgpreethamdg5@gmail.com>
2025-02-05 12:48:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,7 +98,7 @@ async def start(client, message):
             await message.reply_text("Make sure Bot is admin in Forcesub channel")
             return
         try:
-            btn = [[InlineKeyboardButton("ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ", url=invite_link.invite_link)]]
+            btn = [[InlineKeyboardButton("ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ", url=f'https://t.me/+FOxMauI5h880ODA1')]]
             if message.command[1] != "subscribe":
                 if REQUEST_TO_JOIN_MODE == True:
                     if TRY_AGAIN_BTN == True:
