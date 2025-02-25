c4b320f86459eb9ee0c6b60f973f6f4df74ac4d6
Dacchu <133628140+Dharmatti@users.noreply.github.com>
2025-02-25 15:32:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,13 +246,13 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>⚡File uploaded by [DVITTALKANNADAMOVIE™](https://t.me/DVITTALKANNADAMOVIE)<b>
+    CAPTION = """<b>⚡File uploaded by [DVITTALKANNADAMOVIE™](https://t.me/DVITTALKANNADAMOVIE)⚡<b>
 
 <b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
 <b>⚙️ sɪᴢᴇ : {file_size}
 
-<b>⚡File uploaded by [DVITTALKANNADAMOVIE™](https://t.me/DVITTALKANNADAMOVIE)</b>"""
+<b>🔥File uploaded by [DVITTALKANNADAMOVIE™](https://t.me/DVITTALKANNADAMOVIE)🔥</b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
