2b0cc7191e3978141bebe29100fbc78c70d42b92
Dacchu <133628140+Dharmatti@users.noreply.github.com>
2025-02-26 11:51:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '9a744174cdc4c1db7f79d3c4819e58e39cda8cf3')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtoopenlinkvideo') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
